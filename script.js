96affec6c2e059d64acff8a18b46f41ad68f4b4d
GuyFreeman1 <guyfree1991@gmail.com>
2023-05-13 11:42:07 +0300

    week4

--- script.js
+++ script.js
@@ -37,48 +37,70 @@ function calculate(btn) {
   }
 }
 
-
-  function results() {
-    let screen = document.getElementById("screentext");
-    let result;
-  
-    // Convert operands to numbers
-    let num1 = Number(operand1);
-    let num2 = Number(operand2);
-  
-    // Check for divide by zero error
-    if (operator === '/' && num2 === 0) {
-      screen.value = "Error: Divide by zero";
-      return;
+function addDecimal() {
+  let screen = document.getElementById("screentext");
+  if (operator === null) {
+    if (operand1 === null) {
+      operand1 = "0.";
+    } else if (operand1.indexOf(".") === -1) {
+      operand1 += ".";
     }
-  
-    // Calculate result based on operator
-    switch (operator) {
-      case "+":
-        result = num1 + num2;
-        break;
-      case "-":
-        result = num1 - num2;
-        break;
-      case "*":
-        result = num1 * num2;
-        break;
-      case "/":
-        result = num1 / num2;
-        break;
+    screen.value = operand1;
+  } else {
+    if (operand2 === null) {
+      operand2 = "0.";
+    } else if (operand2.indexOf(".") === -1) {
+      operand2 += ".";
     }
-  
-    // Limit result to 6 decimal places
-    result = result.toFixed(6);
-  
-    // Display result
-    screen.value = result;
-  
-    // Reset state
-    operand1 = result.toString();
-    operand2 = null;
-    operator = null;
+    screen.value = operand1 + operator + operand2;
   }
+}
+
+function results() {
+  let screen = document.getElementById("screentext");
+  let result;
+
+  // Convert operands to numbers
+  let num1 = Number(operand1);
+  let num2 = Number(operand2);
+
+  // Check for divide by zero error
+  if (operator === '/' && num2 === 0) {
+    screen.value = "Error: Divide by zero";
+    return;
+  }
+
+  // Calculate result based on operator
+  switch (operator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+  }
+
+  // If result is not decimal, show as a full number
+  if (Number.isInteger(result)) {
+    result = result.toFixed(0);
+  } else {
+    result = result.toFixed(6);
+  }
+
+  // Display result
+  screen.value = result;
+
+  // Reset state
+  operand1 = result.toString();
+  operand2 = null;
+  operator = null;
+}
 
 function clr() {
   let screen = document.getElementById("screentext");
@@ -94,3 +116,63 @@ function addOperator(value) {
   let screen = document.getElementById("screentext");
   screen.value += operator;
 }
+
+document.addEventListener('keydown', function(event) {
+  const key = event.key;
+  switch(key) {
+    case '0':
+      calculate(0);
+      break;
+    case '1':
+      calculate(1);
+      break;
+    case '2':
+      calculate(2);
+      break;
+    case '3':
+      calculate(3);
+      break;
+    case '4':
+      calculate(4);
+      break;
+    case '5':
+      calculate(5);
+      break;
+    case '6':
+      calculate(6);
+      break;
+    case '7':
+      calculate(7);
+      break;
+    case '8':
+      calculate(8);
+      break;
+    case '9':
+      calculate(9);
+      break;
+    case '.':
+      addDecimal('.');
+      break;
+    case '+':
+      addOperator('+');
+      break;
+    case '-':
+      addOperator('-');
+      break;
+    case '*':
+      addOperator('*');
+      break;
+    case '/':
+      addOperator('/');
+      break;
+    case '=':
+      results();
+      break;
+    case 'Enter':
+      results();
+      break;
+    case 'Backspace':
+      clr();
+      break;
+  }
+});
